278fecd5d6ce4eba4af3004af7cd8f44a1e1504f
Iryna Babych <wilyreptiloid@gmail.com>
2024-05-07 11:46:15 +0300

    Added problem class and class for nn

---
+++ app/neural_network.py
@@ -0,0 +1,27 @@
+import torch
+from torch import nn
+
+
+class Approximator(nn.Module):
+    def __init__(self, *,
+                 num_inputs = 2,
+                 num_neurons = 5,
+                 num_layers = 3,
+                 activation = nn.Tanh(),
+                 num_outputs = 1):
+
+        super().__init__()
+
+        start_layer = nn.Linear(num_inputs, num_neurons)
+        layers = [start_layer, activation]
+        for _ in range(num_layers):
+            layers.extend([nn.Linear(num_neurons, num_neurons), activation])
+
+        output_layer = nn.Linear(num_neurons, num_outputs)
+        layers.append(output_layer)
+
+        self.network = nn.Sequential(*layers)
+
+    def forward(self, t, x):
+        x = self.network(torch.vstack((t, x)).T)
+        return torch.squeeze(x)
---
+++ app/problem.py
@@ -0,0 +1,118 @@
+import sympy as smp
+import util_functions
+
+class Heat():
+    ''' Клас для опису теплового одновимірного диференціального рівняння.
+    u_t = a*u_xx + f(x, t)
+    u(0, x) = g(x) - початкова умова
+    u(t, a) = l(t)  - ліва крайова умова
+    u(t, b) = r(t)  - права крайова умова
+    '''
+    left_x = None       # ліва границя області по х
+    right_x = None      # права границя області по х
+
+    start_t = None      # start of time
+    end_t = None        # end of time count
+    
+    left_boundary = None      # left boundary condition
+    right_boundary = None     # right boundary condition
+    initial = None            # initial condition
+    exact_solution = None           # solution for checking an algorithm
+
+    alpha = None            # coefficient in the equation
+    fn = None               # component of heterogeneousity
+    _text = None             # things for formatted printing
+
+    def __init__(self, 
+                 alpha, 
+                 initial, 
+                 left_b, 
+                 right_b, 
+                 left_x = 0, 
+                 right_x = 1, 
+                 start_t = 0, 
+                 end_t = 1,  
+                 solution = None) -> None:
+        
+        self.alpha = alpha
+
+        self.left_boundary = left_b
+        self.right_boundary = right_b
+        self.right_x = right_x
+        self.left_x = left_x
+
+        self.start_t = start_t
+        self.end_t = end_t
+
+        self.initial = initial
+        self.solution = solution
+
+    def __init__(self, json_data):
+        ''' Reads from json with format:
+    {
+        "number of example to use": 1,
+        "examples": [
+
+           {"left x": 0,       #number
+            "right x": 2,      #number
+            "start t": 0,      #number
+            "end t": 1,        #number
+        
+            "left boundary condition": "0",       # sympy functions
+            "right boundary condition": "0",
+            "initial condition":"2*sin(pi*x/2) - sin(pi*x) + 4*sin(2*pi*x)",
+            "solution":"2*sin(pi*x/2)*exp(-(pi**2)*t/16) - sin(pi*x)*exp(-(pi**2)*t/4) + 4*sin(2*pi*x)*exp(-(pi**2)*t)",
+        
+            "alpha": 0.25},        #also number
+        ]
+    } 
+    '''
+        try: 
+            _ = json_data["number of example to use"] + 1
+        except TypeError:
+            raise TypeError("Number of example to use should be number")
+
+        if json_data["number of example to use"] > len(json_data['examples']) or (json_data["number of example to use"] < 0):
+            raise ValueError("Numbers of example to use is nonexistent.")
+        
+
+        json_data = json_data['examples'][json_data["number of example to use"]]
+        # logger.debug(json_data)
+        
+        self.left_x = json_data["left x"]
+        self.right_x = json_data["right x"]
+        
+        self.start_t = json_data["start t"]
+        self.end_t = json_data["end t"]
+        self.alpha = json_data["alpha"]
+        
+        # logger.debug(f"x0: {self.left_x}")
+        # logger.debug(f"x1: {self.right_x}")
+        # logger.debug(f"t0: {self.start_t}")
+        # logger.debug(f"t1: {self.end_t}")
+        
+        x = smp.Symbol("x")
+        t = smp.Symbol("t")
+        
+        try:
+            self.left_boundary = smp.lambdify((x, t), json_data["left boundary condition"])
+            self.right_boundary = smp.lambdify((x, t), json_data["right boundary condition"])
+            self.initial = smp.lambdify((x, t), json_data["initial condition"])
+            if json_data['solution'] is not None:
+                self.solution = smp.lambdify((x, t), json_data["solution"])
+                # logger.debug(f"solution from (3, 4): {self.solution(3, 4)}")
+            else:
+                self.solution = None
+
+        except NameError as e:
+            # logger.error("Problems were encountered while reading given problem. All conditions should be functions from (x, t), no other letter variables allowed.")
+            # logger.error(e)
+            print(e)
+        except SyntaxError as e:
+            # logger.error("Syntax error. Probably, there is at least 1 unclosed parenthesis.")
+            # logger.error(e)
+            print(e)
+
+        self._text = [util_functions.insert_newline(json_data["left boundary condition"]), 
+                     util_functions.insert_newline(json_data["right boundary condition"]), 
+                     util_functions.insert_newline(json_data["initial condition"])]
